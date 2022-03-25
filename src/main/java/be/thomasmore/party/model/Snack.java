95e11d1ac5de68aff1bbc6ca2fef4e4debbad139
JasperWithofs <withofs.jasper@gmail.com>
2022-03-25 09:15:57 +0100

    oefening 1

---
+++ src/main/java/be/thomasmore/party/model/Snack.java
@@ -0,0 +1,16 @@
+package be.thomasmore.party.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Snack {
+    @Id
+    private Integer id;
+    private String name;
+    private boolean vegan;
+    private boolean sideDishPossible;
+    private double price;
+    private Double priceSideDish;
+
+}
